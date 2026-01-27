9f20c7680cf427f1a9b96b46bd25aa2b24ce935a
Prajwal Wakade <prajwalwakade78@gmail.com>
2026-01-27 20:48:41 +0530

    Add files via upload
    a simple calculator

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("hello world")
+#simple calculator by using arithmatic operator
+a = int(input("enter first number"))
+b = int(input("enter second number"))
+addition = a+b
+print(addition)
+substraction = a-b
+print(substraction)
+multiplication = a*b
+print(multiplication)
+division = a/b
+print(division)
